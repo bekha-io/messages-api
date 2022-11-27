1be77d0ef387d2252a30e4d1c6ea326f79d2335c
Avesto Innovations <avesto.inn@gmail.com>
2022-11-27 15:48:28 +0500

    logging and reload options added in gunicorn_conf.py

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -9,5 +9,10 @@ worker_class = 'uvicorn.workers.UvicornWorker'
 
 # Logging Options
 loglevel = 'debug'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 accesslog = '/var/www/messages-api/logs/access_log'
 errorlog = '/var/www/messages-api/logs/error_log'
+
+# Reload options
+reload = True
+reload_engine = "auto"
